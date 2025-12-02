e1d671cde7847da4d94c5fbd1420a4b00b7ecd78
Juan Pablo Obando <juan.obando@gruponex.com.co>
2025-12-02 17:04:01 -0500

    hello world

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,22 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://[::1]:5173',
+        'http://localhost:8080',
+        'http://localhost:3000',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
